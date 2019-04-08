3aa288eb16492458c0e7a1fe5b68a9858e9e7e6e
leireriel <leirericop@gmail.com>
2019-04-08 08:30:09 +0200

    html done and first function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('>> Readyi :)');
+const button = document.querySelector('.button');
+
+//Function to get random number 0-100
+function getRandomNumber() {
+    const randomNumber = Math.ceil(Math.random() * 100);
+    console.log(randomNumber);
+}
+
+//Function to compare both numbers
+function compareNumbers() {
+    
+}
+
+getRandomNumber();
+
+button.addEventListener('click', compareNumbers);

975db908126a66e90386dcf7482e92ca17381ca9
leireriel <leirericop@gmail.com>
2019-04-08 08:58:27 +0200

    working excepting for exclude number 0

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,18 +1,24 @@
 'use strict';
 
 const button = document.querySelector('.button');
+const input = document.querySelector('.input');
 
-//Function to get random number 0-100
-function getRandomNumber() {
-    const randomNumber = Math.ceil(Math.random() * 100);
+
+//Function to get random number 0-100 and compare with input value
+function getRandomCompared() {
+    const randomNumber = Math.ceil(Math.random() * (100-1) + 1);
     console.log(randomNumber);
+    console.log(input.value);
+    if (randomNumber === input.value) {
+        console.log('You win!!');
+    } else if (randomNumber < input.value) {
+        console.log('Too hight 😜');
+    } else if (randomNumber > input.value) {
+        console.log('Too short 😜');
+    } else if (input.value === "") {
+        console.log('Please, write a valid number');
+    }
 }
 
-//Function to compare both numbers
-function compareNumbers() {
-    
-}
 
-getRandomNumber();
-
-button.addEventListener('click', compareNumbers);
+button.addEventListener('click', getRandomCompared);
